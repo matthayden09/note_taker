2462a8228883a36b8dd09a8dfefd23107cc5832c
Matthew Hayden <matthayden09@gmail.com>
2021-02-01 14:13:28 -0600

    connected html routes to server

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
 const express = require('express');
-
-// create express server
 const app = express();
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use((req, res, next) => {
-    console.log(req.url, res.statusCode)
-    next()
-})
+
+// app.use((req, res, next) => {
+//     console.log(req.url, res.statusCode)
+//     next()
+// })
 
 // require("./routes/apiRoutes")(app);
-// require("./routes/htmlRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
 app.listen(PORT, () => {
     console.log(`Server listening on http://localhost:${PORT}`)
